a52eb32a5a2ddfc59c496fdc3234c8139e228936
Imballinst <lucent_rays@yahoo.com>
2015-06-25 15:41:58 +0700

    bikinan sendiri

---
+++ login2.php
@@ -0,0 +1,76 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <!-- META THINGS -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- STYLESHEET -->
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400;700|Open+Sans:400' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="assets2/css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="assets2/css/style.css"/>
+
+    <!-- BACKGROUND -->
+    <style>
+        body {
+            background: url("assets2/img/wallpaper-0.jpg") no-repeat center center fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+    </style>
+
+    <title>
+        Sistem Informasi Gratifikasi
+    </title>
+</head>
+<body>
+    <div class="row header-logo">
+        <div class="col-md-2"></div>
+        <div class="col-md-8 col-md-8-custom-header">
+            <a href=""><img class="img-header-logo" src="assets/img/kpk-logo.png"></img></a>
+        </div>
+        <div class="col-md-2"></div>
+    </div>
+     <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <div class="row row-custom">
+                        <h2 class="center">Login</h2>
+                        <form>
+                            <div class="col-md-6 col-md-offset-3">
+                                <div class="form-group">
+                                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-md-offset-3">
+                                <div class="form-group">
+                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-md-offset-4">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox"> Biarkan saya tetap masuk
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-md-offset-3">
+                                <button type="submit" class="btn btn-default">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-3"></div>
+            </div>
+        <footer class="footer footer-custom">
+            <span class="footer-left">&copy; Komisi Pemberantasan Korupsi 2015, All Rights Reserved</span>
+            <span class="footer-right">Sistem Informasi Gratifikasi</span>
+            <div class="clear-footer"></div>
+        </footer>
+    </div>
+    <script type="text/javascript" src="assets2/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets2/js/bootstrap.min.js"></script>
+</body>
+</html>
