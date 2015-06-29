98b71dfd7012f6ddd5dbf3061bc0f76010d4d750
Imballinst <lucent_rays@yahoo.com>
2015-06-29 15:57:27 +0700

    update

--- header.php
+++ header.php
@@ -7,13 +7,14 @@
 
     <!-- STYLESHEET -->
     <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400;700|Open+Sans:400' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Righteous' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css"/>
     <link rel="stylesheet" type="text/css" href="assets/css/style.css"/>
 
     <!-- BACKGROUND -->
     <style>
         body {
-            background: url("assets/img/wallpaper-1.jpg") no-repeat center center fixed;
+            background: url("assets/img/wallpaper-3.jpg") no-repeat center center fixed;
             -webkit-background-size: cover;
             -moz-background-size: cover;
             -o-background-size: cover;
@@ -30,6 +31,13 @@
         <div class="col-md-2"></div>
         <div class="col-md-8 col-md-8-custom-header">
             <a href=""><img class="img-header-logo" src="assets/img/kpk-logo.png"></img></a>
+        </div>
+        <div class="col-md-2"></div>
+    </div>
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8 col-md-8-title-text">
+            Sistem Informasi Gratifikasi
         </div>
         <div class="col-md-2"></div>
     </div>
--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include("navbar.php"); ?>
             <div class="col-md-4"></div>
             <div class="col-md-1"></div>
             <div class="col-md-2">
-                <button class="btn btn-primary btn-primary-custom">
+                <button class="btn btn-primary btn-primary-custom" data-toggle="modal" data-target="#tambahPengguna">
                     <span class="glyphicon glyphicon-plus-sign"></span>Tambah Pengguna
                 </button>
             </div>
@@ -76,5 +76,48 @@ include("navbar.php"); ?>
                     </tbody>
                 </table>
             </div>
+        </div>
+
+        <!-- MODAL -->
+        <div class="modal fade" id="tambahPengguna" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+              </div>
+              <div class="modal-body">
+                <form>
+                    <div>
+                        <input type="hidden" id="hidden_id"/>
+                        <input type="hidden" id="id_pengguna"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="input_npp">Nomor Pokok Pegawai</label>
+                        <input class="form-control" type="text" id="input_npp" placeholder="Nomor Pokok Pegawai"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="input_nama_pengguna">Nama Pengguna</label>
+                        <input class="form-control" type="text" id="input_nama_pengguna" placeholder="Nama Pengguna"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="input_display_name">Nama Lengkap</label>
+                        <input class="form-control" type="text" id="input_nama_lengkap" placeholder="Nama Lengkap"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="select_status">Status</label>
+                        <select class="form-control" id="select_status">
+                            <option value="0">Aktif</option>
+                            <option value="1">Tidak Aktif</option>
+                        </select>
+                    </div>
+                </form>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary btn-primary-custom">Save</button>
+              </div>
+            </div>
+          </div>
         </div>
 <?php include("footer.php"); ?>
